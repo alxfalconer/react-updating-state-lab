579bdc5dfa3e0013f7abcd0ab48b5f0ff52e19ac
Alexander <al.x.falconer@gmail.com>
2022-02-28 14:00:00 +0100

    component created

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,36 @@
 // Code YouTubeDebugger Component Here
+import React from "react";
+
+export default class YouTubeDebugger extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p",
+        },
+      },
+    };
+  }
+
+  setResolution = () =>
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: { resolution: "720p" },
+      },
+    });
+
+  render() {
+    return (
+      <div>
+        <button className="resolution" onClick={this.setResolution}>
+          resolution down
+        </button>
+      </div>
+    );
+  }
+}
